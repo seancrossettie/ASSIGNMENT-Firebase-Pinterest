df233e6445afa7dd93fdb0c73130a1279e78610a
Sean <jb@JBs-MacBook-Pro.local>
2021-03-06 16:11:28 -0600

    fixed nav-bar styling

--- src/javascripts/components/navBar.js
+++ src/javascripts/components/navBar.js
@@ -1,27 +1,18 @@
 const navBar = () => {
-  document.querySelector('#navigation').innerHTML = `<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-transparent">
-                                                        <button 
-                                                          class="navbar-toggler" 
-                                                          type="button" 
-                                                          data-bs-toggle="collapse" 
-                                                          data-bs-target="#navbarSupportedContent" 
-                                                          aria-controls="navbarSupportedContent" 
-                                                          aria-expanded="false" 
-                                                          aria-label="Toggle navigation"
-                                                        >
-                                                          <span class="navbar-toggler-icon"></span>
-                                                        </button>
+  document.querySelector('#navigation').innerHTML = `<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-transparent d-flex flex-nowrap">
                                                         <div class="collapse navbar-collapse me-2" id="navbar">
-                                                        <button class="btn" <img src="./documentation/pinterest-logo-png-1982.png" alt=""></button>
-                                                        <ul class="navbar-nav mr-auto">
+                                                          <a href="#">
+                                                            <img class="mx-2 rounded-pill" src="./documentation/pinterest-logo-png-1982.png" alt="" id="corner-logo">
+                                                          </a>
+                                                        <ul class="navbar-nav">
                                                           <li class="nav-item active">
-                                                            <button class="btn rounded-pill me-2" href="#" id="boards-page">Boards</button>
+                                                            <button class="btn rounded-pill" href="#" id="boards-page">Boards</button>
                                                           </li>
                                                           <li class="nav-item active">
-                                                            <button class="btn rounded-pill me-2" href="#" id="pins-page">Pins</button>
+                                                            <button class="btn rounded-pill" href="#" id="pins-page">Pins</button>
                                                           </li>
                                                           <li class="nav-item active">
-                                                            <button class="btn rounded-pill me-2" href="#" id="favorites-page">Favorites</button>
+                                                            <button class="btn rounded-pill" href="#" id="favorites-page">Favorites</button>
                                                           </li>
                                                         </ul>
                                                         <div class="wrapper mr-auto" id="search-bar">
