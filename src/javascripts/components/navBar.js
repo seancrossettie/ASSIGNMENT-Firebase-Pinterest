fab6adb1bf547ad382aae59e9deb116764d537c2
Sean <jb@JBs-MacBook-Pro.local>
2021-03-06 13:55:16 -0600

    fixed navbar styling

--- src/javascripts/components/navBar.js
+++ src/javascripts/components/navBar.js
@@ -1,10 +1,26 @@
 const navBar = () => {
-  document.querySelector('#navigation').innerHTML = `<nav class="navbar navbar-light bg-">
-                                                      <div class="container-fluid">
-                                                        <a class="navbar-brand" href="#">
-                                                          <img src="https://pngimg.com/uploads/pinterest/pinterest_PNG66.png" alt="" width="50" height="30" class="d-inline-block align-top">
-                                                          Pinterest
-                                                        </a>
+  document.querySelector('#navigation').innerHTML = `<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-transparent">
+                                                        <a class="navbar-brand" href="#">Pretty Much Pinterest</a>
+                                                        <button 
+                                                          class="navbar-toggler" 
+                                                          type="button" 
+                                                          data-bs-toggle="collapse" 
+                                                          data-bs-target="#navbarSupportedContent" 
+                                                          aria-controls="navbarSupportedContent" 
+                                                          aria-expanded="false" 
+                                                          aria-label="Toggle navigation"
+                                                        >
+                                                          <span class="navbar-toggler-icon"></span>
+                                                        </button>
+                                                        <div class="collapse navbar-collapse" id="navbarNav">
+                                                        <ul class="navbar-nav mr-auto">
+                                                          <li class="nav-item active">
+                                                            <a class="nav-link" href="#" id="board-page">Boards</a>
+                                                          </li>
+                                                          <li class="nav-item active">
+                                                            <a class="nav-link" href="#" id="pins-page">Pins</a>
+                                                          </li>
+                                                        </ul>
                                                         <div id="logout-button"></div>
                                                       </div>
                                                     </nav>
