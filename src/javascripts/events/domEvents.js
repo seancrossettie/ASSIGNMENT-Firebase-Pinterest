0c09a35e1d67765ed3b16d27adb737e97c0cd8c6
Sean <jb@JBs-MacBook-Pro.local>
2021-03-08 21:08:17 -0600

    delete pins and board function

--- src/javascripts/events/domEvents.js
+++ src/javascripts/events/domEvents.js
@@ -1,16 +1,14 @@
-// import firebase from 'firebase/app';
-// import 'firebase/auth';
-import { deleteBoards } from '../helpers/data/boardData';
 import showBoards from '../components/showBoards';
 import { deletePins } from '../helpers/data/pinData';
 import { showPins } from '../components/showPins';
+import deleteBoardPins from '../helpers/data/boardPinData';
 
 const domEvents = () => {
   document.querySelector('body').addEventListener('click', (e) => {
-    // CLICK EVENT FOR DELETING A BOARD
+    // CLICK EVENT FOR DELETING A BOARD AND ALL ASSOCIATED PINS
     if (e.target.id.includes('delete-board')) {
-      const firebaseKey = e.target.id.split('--')[1];
-      deleteBoards(firebaseKey).then((boardsArray) => showBoards(boardsArray));
+      const boardId = e.target.id.split('--')[1];
+      deleteBoardPins(boardId).then((boardsArray) => showBoards(boardsArray));
     }
     // CLICK EVENT FOR DELETEING A PIN
     if (e.target.id.includes('delete-pin')) {
--- src/javascripts/helpers/data/pinData.js
+++ src/javascripts/helpers/data/pinData.js
@@ -22,4 +22,10 @@ const deletePins = (firebaseKey) => new Promise((resolve, reject) => {
     .catch((error) => reject(error));
 });
 
-export { getPins, deletePins };
+const getBoardPins = (boardId) => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/pins.json?orderBy="board_id"&equalTo="${boardId}"`)
+    .then((response) => resolve(Object.values(response.data)))
+    .catch((error) => reject(error));
+});
+
+export { getPins, deletePins, getBoardPins };
