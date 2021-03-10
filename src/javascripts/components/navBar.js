16dd30e8382f762c7f79058c9b0d5c8fcf0efb4a
Sean <jb@JBs-MacBook-Pro.local>
2021-03-09 20:24:34 -0600

    added create boards functions

--- src/javascripts/components/navBar.js
+++ src/javascripts/components/navBar.js
@@ -1,44 +1,46 @@
 const navBar = () => {
-  document.querySelector('#navigation').innerHTML = `<nav class="navbar fixed-top navbar-expand-lg navbar-light mt-3">
-                                                      <img class="mx-2 rounded-pill" src="./documentation/pinterest-logo-png-1982.png" alt="" id="corner-logo">
-                                                      <button
-                                                        class="navbar-toggler"
-                                                        type="button"
-                                                        data-toggle="collapse"
-                                                        data-target="#navbarNav"
-                                                        aria-controls="navbarNav"
-                                                        aria-expanded="false"
-                                                        aria-label="Toggle navigation"
-                                                      >
-                                                        <span class="navbar-toggler-icon"></span>
-                                                      </button>
-                                                      <div class="collapse navbar-collapse" id="navbarNav">
-                                                        <ul class="navbar-nav mr-auto">
-                                                          <li class="nav-item">
-                                                              <button class="btn rounded-pill m" href="#" id="boards-page">Boards</button>
-                                                          </li>
-                                                          <li class="nav-item">
-                                                              <button class="btn rounded-pill" href="#" id="pins-page">Pins</button>
-                                                          </li>
-                                                          <li class="nav-item">
-                                                              <button class="btn rounded-pill" href="#" id="favorites-page">Favorites</button>
-                                                          </li>
-                                                          <li class="nav-item" id="create-button"></li>
-                                                          <div class="wrapper mr-auto" id="search-bar">
-                                                            <form>
-                                                              <input
-                                                                type="text"
-                                                                id="search-bar-text"
-                                                                class="form-control rounded-pill"
-                                                                placeholder="Search"
-                                                                aria-label="Search"
-                                                              >
-                                                            </form>
-                                                          </div>
-                                                        </ul>
-                                                        <div id="logout-button"></div>
-                                                      </div>
-                                                    </nav>`;
+  document.querySelector('#navigation').innerHTML = `
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light mt-3">
+      <img class="mx-2 rounded-pill" src="./documentation/pinterest-logo-png-1982.png" alt="" id="corner-logo">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav mr-2">
+          <li class="nav-item">
+              <button class="btn rounded-pill m" href="#" id="boards-page">Boards</button>
+          </li>
+          <li class="nav-item">
+              <button class="btn rounded-pill" href="#" id="pins-page">Pins</button>
+          </li>
+          <li class="nav-item">
+              <button class="btn rounded-pill" href="#" id="favorites-page">Favorites</button>
+          </li>
+          <li class="nav-item" id="create-button"></li>
+        </ul>
+        <div class="wrapper mr-auto" id="search-bar">
+          <form>
+            <input
+              type="text"
+              id="search-bar-text"
+              class="form-control rounded-pill"
+              placeholder="Search"
+              aria-label="Search"
+            >
+          </form>
+        </div>
+        <div id="logout-button"></div>
+      </div>
+    </nav>
+    `;
 };
 
 export default navBar;
